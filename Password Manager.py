fc8d47268c6b49e69b20e1e47ebc1bc93ab3d2a1
PHUMM1N <144439408+PHUMM1N@users.noreply.github.com>
2023-09-14 22:02:49 +0700

    Update Password Manager.py

--- Password Manager.py
+++ Password Manager.py
@@ -5,17 +5,17 @@ from tkinter import simpledialog
 
 root = Tk()
 root.title("Password Manager")
-root.configure(bg="#E8f1fd")  # Set the background color for the root window
+root.configure(bg="#E8f1fd")  
 background_image = Label(root, bg="#E8F1FD")
 background_image.grid(row=0, column=0, columnspan=2)  
 
 # Database file path
 db_file_path = r"D:\python work\security.db"
-logged_in_user_id = None  # Variable to store the logged-in user's ID
+logged_in_user_id = None  
 collected_data = []
-websites_listbox = None  # Declare websites_listbox as a global variable
+websites_listbox = None  
 
-# Function to center the window on the screen
+
 def center_window(window, width, height):
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
@@ -23,7 +23,7 @@ def center_window(window, width, height):
     y = (screen_height / 2) - (height / 2)
     window.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
 
-# Create user
+
 def create_user():
     conn = sqlite3.connect(db_file_path)
     cursor = conn.cursor()
@@ -36,7 +36,7 @@ def create_user():
     regis_password_entry.delete(0, END)
     messagebox.showinfo("Create User", "User created successfully!")
 
-# Create tables
+
 def create_table():
     conn = sqlite3.connect(db_file_path)
     cursor = conn.cursor()
@@ -49,7 +49,7 @@ def create_table():
     conn.commit()
     conn.close()
 
-# Load collected data
+
 def load_collected_data():
     global collected_data
     collected_data.clear()
@@ -61,7 +61,7 @@ def load_collected_data():
     for row in rows:
         collected_data.append(row)
 
-# Login
+
 def login():
     global logged_in_user_id
     conn = sqlite3.connect(db_file_path)
@@ -76,18 +76,18 @@ def login():
         logged_in_user_id = user_id[0]
         messagebox.showinfo("Login Successful", "Login successful!")
         root.withdraw()
-        load_collected_data()  # Load data for the logged-in user
+        load_collected_data()  
         show_main_window1()
     else:
         messagebox.showerror("Error", "Login failed. Please check your username and password.")
 
-# Delete password window
+
 def delete_password_window():
-    global collected_data  # Declare collected_data as global
+    global collected_data  
 
     delete_window = Toplevel()
     delete_window.title("Delete Password")
-    delete_window.configure(bg="#E8f1fd")  # Set background color
+    delete_window.configure(bg="#E8f1fd") 
 
     def delete_selected_password():
         selected_index = passwords_listbox.curselection()
@@ -95,13 +95,13 @@ def delete_password_window():
             messagebox.showwarning("No Selection", "Please select a password entry to delete.")
             return
 
-        selected_details = passwords_listbox.get(selected_index[0])  # Get the selected entry's details
-        selected_website = selected_details.split(',')[0].split(': ')[1].strip()  # Extract the website
+        selected_details = passwords_listbox.get(selected_index[0])  
+        selected_website = selected_details.split(',')[0].split(': ')[1].strip()  
 
         conn = sqlite3.connect(db_file_path)
         cursor = conn.cursor()
 
-        # Retrieve the user_id based on the logged-in user's username
+        
         cursor.execute('SELECT rowid FROM users WHERE username=?', (username_entry.get(),))
         user_id = cursor.fetchone()[0]
 
@@ -109,7 +109,7 @@ def delete_password_window():
         conn.commit()
         conn.close()
 
-        # Create a copy of the collected_data list to avoid modification during iteration
+       
         collected_data_copy = collected_data.copy()
         for item in collected_data_copy:
             if item[0] == selected_website:
@@ -118,24 +118,24 @@ def delete_password_window():
         passwords_listbox.delete(selected_index)
         messagebox.showinfo("Delete Password", f"Password for '{selected_website}' deleted successfully!")
 
-    # Create a listbox to display collected passwords
-    passwords_listbox = Listbox(delete_window, height=20, width=60, bg="#FFFFFF")  # Set background color
+    
+    passwords_listbox = Listbox(delete_window, height=20, width=60, bg="#FFFFFF") 
     passwords_listbox.pack(fill=BOTH, expand=True)
 
-    # Insert the collected passwords with details into the listbox
+   
     for item in collected_data:
         passwords_listbox.insert(END, f"Website: {item[0]}, Username: {item[1]}, Password: {item[2]}")
 
-    delete_button = Button(delete_window, text="Delete Password", command=delete_selected_password, bg="#FF6347", fg="white")  # Set button color
+    delete_button = Button(delete_window, text="Delete Password", command=delete_selected_password, bg="#FF6347", fg="white")  
     delete_button.pack()
 
-# Change selected password window
+
 def change_selected_password_window():
     global collected_data
 
     change_window = Toplevel()
     change_window.title("Change Password")
-    change_window.configure(bg="white")  # Set background color
+    change_window.configure(bg="white")  
 
     def change_selected_password():
         selected_index = passwords_listbox.curselection()
@@ -179,36 +179,36 @@ def change_selected_password_window():
 
                 messagebox.showinfo("Change Password", "Password updated successfully!")
 
-    passwords_listbox = Listbox(change_window, height=20, width=60, bg="#FFFFFF")  # Set background color
+    passwords_listbox = Listbox(change_window, height=20, width=60, bg="#FFFFFF") 
     passwords_listbox.pack(fill=BOTH, expand=True)
 
     for item in collected_data:
         passwords_listbox.insert(END, f"Website🌍: {item[0]}  Username 👤: {item[1]} Password🗝: {item[2]}",)
 
 
-    change_button = Button(change_window, text="Change Password", command=change_selected_password, bg="#4CAF50", fg="white")  # Set button color
+    change_button = Button(change_window, text="Change Password", command=change_selected_password, bg="#4CAF50", fg="white")  
     change_button.pack()
 
 def show_main_window1():
     main_window = Toplevel()
     main_window.title("Password Manager")
-    main_window.configure(bg="#E8f1fd")  # Set background color
+    main_window.configure(bg="#E8f1fd")  
 
-    # Set the size of the main window
+  
     main_window.geometry("400x300")
     center_window(main_window, 400, 300)
     menu= Label(main_window, text="MENU",width=50).pack()
 
-    import_button = Button(main_window, text="Add Password ➕", command=show_import, width=20, bg="#FFFAFA", fg="Black")  # Set button color
+    import_button = Button(main_window, text="Add Password ➕", command=show_import, width=20, bg="#FFFAFA", fg="Black")  
     import_button.pack(pady=10)
 
-    search_button = Button(main_window, text="Search Passwords 🔍", command=search_data, width=20, bg="#FFFAFA", fg="Black")  # Set button color
+    search_button = Button(main_window, text="Search Passwords 🔍", command=search_data, width=20, bg="#FFFAFA", fg="Black")  
     search_button.pack()
 
-    change_password_button = Button(main_window, text="Change Password 🔧", command=change_selected_password_window, width=20, bg="#FFFAFA", fg="Black")  # Set button color
+    change_password_button = Button(main_window, text="Change Password 🔧", command=change_selected_password_window, width=20, bg="#FFFAFA", fg="Black")  
     change_password_button.pack(pady=10)
 
-    delete_password_button = Button(main_window, text="Delete Password 🗑", command=delete_password_window, width=20, bg="#FFFAFA", fg="Black")  # Set button color
+    delete_password_button = Button(main_window, text="Delete Password 🗑", command=delete_password_window, width=20, bg="#FFFAFA", fg="Black")  
     delete_password_button.pack()
 
     
@@ -229,19 +229,19 @@ def import_data(lead_window, import_website_entry, import_username_entry, import
     import_password_entry.delete(0, END)
     collected_data.append((website_add, username_add, password_add))
     messagebox.showinfo("Import Data", "Data imported successfully!")
-    lead_window.destroy()  # Close the import window after saving the data
+    lead_window.destroy()  
 
-# Search data
+
 def search_data():
     search_window = Toplevel()
     search_window.title("Search Passwords")
-    search_window.configure(bg="#E8f1fd")  # Set background color
+    search_window.configure(bg="#E8f1fd")  
 
-    # Set the size of the search window
+   
     search_window.geometry("400x300")
     center_window(search_window, 400, 300)
 
-    data_listbox = Listbox(search_window, height=15, width=60, bg="#FFFFFF")  # Set background color
+    data_listbox = Listbox(search_window, height=15, width=60, bg="#FFFFFF")  
     data_listbox.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
     for item in collected_data:
@@ -249,30 +249,27 @@ def search_data():
 
     search_window.mainloop()
 
-# Show import window
-# ...
 
-# ...
 
 def show_import():
     lead_window = Toplevel()
     lead_window.title("Add Password")
 
-    # Set the size of the import window
+    
     lead_window.geometry("400x200")
     center_window(lead_window, 400, 200)
 
-    import_website_label = Label(lead_window, text="Website 🌎:", font=("Arial", 12), bg="#E6E6E6")  # Set background color
+    import_website_label = Label(lead_window, text="Website 🌎:", font=("Arial", 12), bg="#E6E6E6")  
     import_website_label.grid(row=0, column=0, padx=10, pady=5)
     import_website_entry = Entry(lead_window, width=30, font=("Arial", 12))
     import_website_entry.grid(row=0, column=1, padx=10, pady=5)
 
-    import_username_label = Label(lead_window, text="Username 👤:", font=("Arial", 12), bg="#E6E6E6")  # Set background color
+    import_username_label = Label(lead_window, text="Username 👤:", font=("Arial", 12), bg="#E6E6E6")  
     import_username_label.grid(row=1, column=0, padx=10, pady=5)
     import_username_entry = Entry(lead_window, width=30, font=("Arial", 12))
     import_username_entry.grid(row=1, column=1, padx=10, pady=5)
 
-    import_password_label = Label(lead_window, text="Password 🗝:", font=("Arial", 12), bg="#E6E6E6")  # Set background color
+    import_password_label = Label(lead_window, text="Password 🗝:", font=("Arial", 12), bg="#E6E6E6")  
     import_password_label.grid(row=2, column=0, padx=10, pady=5)
     import_password_entry = Entry(lead_window, width=30, font=("Arial", 12))
     import_password_entry.grid(row=2, column=1, padx=10, pady=5)
@@ -282,26 +279,26 @@ def show_import():
 
 create_table()
 
-username_label = Label(root, text="Username 👤:", font=("Arial", 12))  # Set background color
+username_label = Label(root, text="Username 👤:", font=("Arial", 12))  
 username_label.grid(row=0, column=0, padx=10, pady=5)
 username_entry = Entry(root, font=("Arial", 12))
 username_entry.grid(row=0, column=1, padx=10, pady=5)
-password_label = Label(root, text="Password 🗝:", font=("Arial", 12) )  # Set background color
+password_label = Label(root, text="Password 🗝:", font=("Arial", 12) )  
 password_label.grid(row=1, column=0, padx=10, pady=5)
 password_entry = Entry(root, show="*", font=("Arial", 12))
 password_entry.grid(row=1, column=1, padx=10, pady=5)
-login_button = Button(root, text="Login", command=login, width=20, font=("Arial", 12), bg="#99CCFF", fg="#000000")  # Set button color
+login_button = Button(root, text="Login", command=login, width=20, font=("Arial", 12), bg="#99CCFF", fg="#000000") 
 login_button.grid(row=2, column=1, padx=10, pady=10)
 
-regis_username = Label(root, text="New username 👤:", font=("Arial", 12), )  # Set background color
+regis_username = Label(root, text="New username 👤:", font=("Arial", 12), )  
 regis_username.grid(row=3, column=0, padx=10, pady=5)
 regis_username_entry = Entry(root, font=("Arial", 12))
 regis_username_entry.grid(row=3, column=1, padx=10, pady=5)
-regis_password = Label(root, text="Password 🗝:", font=("Arial", 12), )  # Set background color
+regis_password = Label(root, text="Password 🗝:", font=("Arial", 12), )  
 regis_password.grid(row=4, column=0, padx=10, pady=5)
 regis_password_entry = Entry(root, font=("Arial", 12))
 regis_password_entry.grid(row=4, column=1, padx=10, pady=5)
-regis_button = Button(root, text="Register", command=create_user, width=20, font=("Arial", 12), bg="#FFE4B5", fg="#000000")  # Set button color
+regis_button = Button(root, text="Register", command=create_user, width=20, font=("Arial", 12), bg="#FFE4B5", fg="#000000")  
 regis_button.grid(row=5, column=1, padx=10, pady=10)
 
 root.geometry("400x300")
